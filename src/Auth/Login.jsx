7ad79796103a1d51c8d438cfd66f3bb96a7b90fe
ayush007-verma <ayush3528.be20@chitkara.edu.in>
2023-10-27 14:34:55 +0530

    /login working with backend receiving success response if user logged in with correct credentiasl

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,8 +1,26 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 import "./../css/Register.css"
+import { useState } from "react"
+import axios from "axios"
 
 const Login = () => {
     const navigate = useNavigate()
+
+    const [loginEmail, setLoginEmail] = useState()
+    const [loginPassword, setLoginPassword] = useState()
+    
+    const postLoginRequest = async() => {
+
+        const loginUser = {
+            email : loginEmail, 
+            password : loginPassword
+        }
+
+        await axios.post("http://localhost:8080/login", loginUser)
+            .then((res) => console.log(res.data))
+            .catch((err) => console.log(err))
+    }
+
     return (
         <main>
 
@@ -13,11 +31,10 @@ const Login = () => {
                     <h2> &nbsp; Login Reddit </h2>
                 </div>
                 
-                
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+                <input type="email" name="email" onChange={(e)=>setLoginEmail(e.target.value)} placeholder="Email" />
+                <input type="password" name="password" onChange={(e)=>setLoginPassword(e.target.value)}  placeholder="Password" />
 
-                <button type="submit"> Login..</button>
+                <button type="submit" onClick={(e) => { e.preventDefault(); postLoginRequest() }}> Login..</button>
 
                 <span> Not a Registered User ?? <a onClick={()=>navigate("/register")}> Register </a></span>
 
