f3f92847cbef365eaa5a3aeb5cb1f8b2ca43c859
ayush007-verma <ayush3528.be20@chitkara.edu.in>
2023-10-29 16:31:15 +0530

    Rearranged folder structure..

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
-import Header from './components/Header'
-import Main from './components/Main'
-import Register from './Auth/Register'
-import Login from './Auth/Login'
-import Home from './test/Home'
-
+import Home from './Home/Home'
 
 function App() {
 
@@ -18,9 +11,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<><Home/></>} />
-          {/* <Route path="/" element={<><Header showHeaderOptions={true}/><Main/></>}/>
-          <Route path="/login" element={<><Header showHeaderOptions={false}/><Login/></>}/>
-          <Route path="/register" element={<><Header showHeaderOptions={false}/><Register/></>}/> */}
         </Routes>
       </BrowserRouter>
 
