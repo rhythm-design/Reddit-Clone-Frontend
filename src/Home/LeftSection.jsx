ff009cff175cb36897d3d88c690751aa2a19031e
rhythm-design <rhythmrv3@gmail.com>
2023-11-01 13:52:22 +0530

    Added categories

--- src/Home/LeftSection.jsx
+++ src/Home/LeftSection.jsx
@@ -1,69 +1,77 @@
 const LeftSection = () => {
     return (
         <>
-            <div class="w-1/3 ml-5">
-                <div class="py-2">
+            <div class="w-1/3 ml-5" >
+                <div class="py-2" style={{height: "auto", border: '2px solid darkgray'}}>
                    
                     <div class="rounded bg-white mb-4">
-                        <div class="p-3 text-xxs font-semibold w-full">TRENDING COMMUNITIES</div>
+                        <div class="p-3 text-xxs font-semibold w-full">TRENDING TOPICS</div>
                         <div class="pb-4">
                             <div class="px-3 py-2">
                                 <div class="flex">
-                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://avatars0.githubusercontent.com/u/30317862?s=200&v=4" />
+                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://icons.veryicon.com/png/o/commerce-shopping/icon-of-lvshan-valley-mobile-terminal/home-category.png" />
                                     <div class="flex flex-col font-medium">
-                                        <a href="#" class="text-xs text-black-alt no-underline leading-tight">r/tailwind</a>
-                                        <span class="text-xxs">1.000 subscribers</span>
-                                    </div>
-                                    <div class="flex ml-auto">
-                                        <button class="bg-blue-dark text-xs text-white font-semibold rounded px-4 ml-auto">SUBSCRIBE</button>
+                                        <span class="text-xxs">Gaming</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="px-3 py-2">
                                 <div class="flex">
-                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://avatars0.githubusercontent.com/u/30317862?s=200&v=4" />
+                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://icons.veryicon.com/png/o/commerce-shopping/icon-of-lvshan-valley-mobile-terminal/home-category.png" />
                                     <div class="flex flex-col font-medium">
-                                        <a href="#" class="text-xs text-black-alt no-underline leading-tight">r/tailwind</a>
-                                        <span class="text-xxs">1.000 subscribers</span>
-                                    </div>
-                                    <div class="flex ml-auto">
-                                        <button class="bg-blue-dark text-xs text-white font-semibold rounded px-4 ml-auto">SUBSCRIBE</button>
+                                        <span class="text-xxs">Sports</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="px-3 py-2">
                                 <div class="flex">
-                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://avatars0.githubusercontent.com/u/30317862?s=200&v=4" />
+                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://icons.veryicon.com/png/o/commerce-shopping/icon-of-lvshan-valley-mobile-terminal/home-category.png" />
                                     <div class="flex flex-col font-medium">
-                                        <a href="#" class="text-xs text-black-alt no-underline leading-tight">r/tailwind</a>
-                                        <span class="text-xxs">1.000 subscribers</span>
-                                    </div>
-                                    <div class="flex ml-auto">
-                                        <button class="bg-blue-dark text-xs text-white font-semibold rounded px-4 ml-auto">SUBSCRIBE</button>
+                                        <span class="text-xxs">Entertainment</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="px-3 py-2">
                                 <div class="flex">
-                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://avatars0.githubusercontent.com/u/30317862?s=200&v=4" />
+                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://icons.veryicon.com/png/o/commerce-shopping/icon-of-lvshan-valley-mobile-terminal/home-category.png" />
                                     <div class="flex flex-col font-medium">
-                                        <a href="#" class="text-xs text-black-alt no-underline leading-tight">r/tailwind</a>
-                                        <span class="text-xxs">1.000 subscribers</span>
-                                    </div>
-                                    <div class="flex ml-auto">
-                                        <button class="bg-blue-dark text-xs text-white font-semibold rounded px-4 ml-auto">SUBSCRIBE</button>
+                                        {/* <a href="#" class="text-xs text-black-alt no-underline leading-tight">r/tailwind</a> */}
+                                        <span class="text-xxs">Business</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="px-3 py-2">
                                 <div class="flex">
-                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://avatars0.githubusercontent.com/u/30317862?s=200&v=4" />
+                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://icons.veryicon.com/png/o/commerce-shopping/icon-of-lvshan-valley-mobile-terminal/home-category.png" />
                                     <div class="flex flex-col font-medium">
-                                        <a href="#" class="text-xs text-black-alt no-underline leading-tight">r/tailwind</a>
-                                        <span class="text-xxs">1.000 subscribers</span>
+                                        <span class="text-xxs">Education</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="px-3 py-2">
+                                <div class="flex">
+                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://icons.veryicon.com/png/o/commerce-shopping/icon-of-lvshan-valley-mobile-terminal/home-category.png" />
+                                    <div class="flex flex-col font-medium">
+                                        <span class="text-xxs">Politics</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="px-3 py-2">
+                                <div class="flex">
+                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://icons.veryicon.com/png/o/commerce-shopping/icon-of-lvshan-valley-mobile-terminal/home-category.png" />
+                                    <div class="flex flex-col font-medium">
+                                        <span class="text-xxs">Technology</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="px-3 py-2">
+                                <div class="flex">
+                                    <img class="h-8 w-8 border rounded-full mr-2" src="https://icons.veryicon.com/png/o/commerce-shopping/icon-of-lvshan-valley-mobile-terminal/home-category.png" />
+                                    <div class="flex flex-col font-medium">
+                                        <span class="text-xxs">Lifestyle</span>
                                     </div>
                                     <div class="flex ml-auto">
-                                        <button class="bg-blue-dark text-xs text-white font-semibold rounded px-4 ml-auto">SUBSCRIBE</button>
+                                        <p class="bg-blue-dark text-xs text-white font-semibold rounded px-4 ml-auto">SUBSCRIBE</p>
                                     </div>
                                 </div>
                             </div>
