69103f2f5e03b7177cfc2f7e820818422bb367d4
Rhythm Varshney <56234578+rhythm-design@users.noreply.github.com>
2023-11-06 06:47:35 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from './services/user-service';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://reddit-clone-backend-production.up.railway.app/api',
   headers: authHeader()
 });
 
